b1d60b76ea6e56a4ad66e2f91b063b8c2bea652a
André Carvalho <afacarvalho@yahoo.com.br>
2020-07-26 01:29:35 -0300

    Divide the project to separate the math module and the usage test. The goal is to publish a package with just the math module.

--- mathModule/fourOperations.js
+++ mathModule/fourOperations.js
@@ -3,19 +3,19 @@ const fourOperations = {
     noNumber: 'NaN',
     forceToNumber: function(n){
         if ((+n).toString()===this.noNumber){throw "Not a Number"}
-        else return n;
+        else return +n;
     },
-    add: function(n1, n2){
+    "+": function(n1, n2){
         return this.forceToNumber(n1) + this.forceToNumber(n2);
     },
-    sub: function(n1, n2){
+    "-": function(n1, n2){
         return this.forceToNumber(n1) - this.forceToNumber(n2);
     },
-    div: function(n1, n2){
+    "/": function(n1, n2){
         if (n2===this.zero){throw "Division by zero"}
         return this.forceToNumber(n1) / this.forceToNumber(n2);
     },
-    mult: function(n1, n2){
+    "*": function(n1, n2){
         return this.forceToNumber(n1) * this.forceToNumber(n2);
     }
 };
--- testMathModule/index.js
+++ testMathModule/index.js
@@ -2,25 +2,25 @@ const mathModule = require("./mathModule/fourOperations");
 
 try {
     // a) 1 + 1
-    console.log("result of item a) "+mathModule.add(1,1));
+    console.log("result of item a) "+mathModule["+"](1,1));
 
     // b) -123 + 123123
-    console.log("result of item b) "+mathModule.add(-123,123123));
+    console.log("result of item b) "+mathModule["+"](-123,123123));
 
     // c) 8 * 0
-    console.log("result of item c) "+mathModule.mult(8,0));
+    console.log("result of item c) "+mathModule["*"](8,0));
 
     // d) 1239123 * 12313
-    console.log("result of item d) "+mathModule.mult(1239123,12313));
+    console.log("result of item d) "+mathModule["*"](1239123,12313));
 
     // e) 123 / -12
-    console.log("result of item e) "+mathModule.div(123,-12));
+    console.log("result of item e) "+mathModule["/"](123,-12));
 
     // f) 313123 / 0
-    console.log("result of item f) "+mathModule.div(313123,0));
+    console.log("result of item f) "+mathModule["/"](313123,0));
 
     // extra "10a" + 3 (is never executed because the previous error is caught)
-    console.log("result of item extra) "+mathModule.div("10a",3));
+    console.log("result of item extra) "+mathModule["/"]("10a",3));
 
 } catch (error) {
     console.log("Error: "+error);
@@ -38,22 +38,22 @@ mathModule.exec=function(operation,n1,n2) {
 };
 
 // a) 1 + 1
-mathModule.exec("add",1,1);
+mathModule.exec("+",1,1);
 
 // b) -123 + 123123
-mathModule.exec("add",-123,123123);
+mathModule.exec("+",-123,123123);
 
 // c) 8 * 0
-mathModule.exec("mult",8,0);
+mathModule.exec("*",8,0);
 
 // d) 1239123 * 12313
-mathModule.exec("mult",1239123,12313);
+mathModule.exec("*",1239123,12313);
 
 // e) 123 / -12
-mathModule.exec("div",123,-12);
+mathModule.exec("/",123,-12);
 
 // f) 313123 / 0
-mathModule.exec("div",313123,0);
+mathModule.exec("/",313123,0);
 
 // extra "10a" + 3 
-mathModule.exec("div","10a",3);
+mathModule.exec("/","10a",3);
